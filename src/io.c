ef177653085a960b8537d196040acb78acdd8a4b
Julien Rodriguez <julien.rodriguez@lirmm.fr>
2026-02-15 11:03:56 +0100

    Refactor io.c for improved documentation and consistency in function naming

--- src/io.c
+++ src/io.c
@@ -22,91 +22,116 @@
 */
 
 
-/************************************************************/
-/**                                                        **/
-/**   NAME       : io.c                                    **/
-/**                                                        **/
-/**   AUTHOR     : Julien RODRIGUEZ                        **/
-/**                                                        **/
-/**   FUNCTION   : These lines are input and output        **/
-/**                functions definitions.                  **/
-/**                                                        **/
-/**   DATES      : # Version 0.0  : from : 10 jun 2022     **/
-/**                                                        **/
-/**                                                        **/
-/**                                                        **/
-/************************************************************/
-
+/** 
+ * @file io.c
+ * @author Julien Rodriguez
+ * @date 10 jun 2022 – 13 oct 2023
+ * @brief  Input and output functions definition for
+ *         read and write red-black hypergraph.    
+ */
  #include"io.h"
 
-/* read a line, removing trailing '\n' */
-int readLine(
-  FILE * file, 
-  char * buffer, 
-  int size) {
-
-
+/**
+ * @brief Read a line and removing trailing '\n'.
+ *
+ * @param file             Input file.
+ * @param buffer           String.
+ * @param size             Size of string. 
+ *
+ * @return The length of the readed line.
+ */
+int 
+read_line(FILE * file, 
+          char * buffer, 
+          int    size) 
+{
   int len = 0;
-  if (fgets(buffer, size, file)) {
-    len = strlen(buffer);
-    FATAL(len == 0, "length is zero");
-    FATAL(buffer[len-1] != '\n', "buffer size too small");
-    buffer[--len] = '\0';
-  }
+
+  if(fgets(buffer, size, file)) 
+    {
+      len = strlen(buffer);
+      FATAL(len == 0, "length is zero");
+      FATAL(buffer[len-1] != '\n', "buffer size too small");
+      buffer[--len] = '\0';
+    }
   return len;
 }
 
-
-int loadPartition(
-  INT i_vertices, 
-  INT * partition, 
-  const char * s_path){
+/**
+ * @brief Read a file describing a partition.
+ *
+ * @param i_vertices       Number of vertices.
+ * @param partition        Partition assignation.
+ * @param s_path           Path of file. 
+ *
+ * @return Integer.
+ */
+int 
+load_partition(INT          i_vertices, 
+               INT        * partition,
+               const char * s_path){
 
   char * buffer = malloc(BUFSIZE);
   MEM_ERROR(buffer);
 
   FILE * in = NULL;
   in = fopen(s_path, "r");
-  if (in == NULL) {
-    fprintf(stderr, "Cannot open file %s\n", s_path);
-    exit(0);
-  }
 
-  for(INT i=0; i < i_vertices; i++) {
-    readLine(in, buffer, BUFSIZE);
-    partition[i] = atoi(buffer);
-  }
+  if (in == NULL) 
+    {
+      fprintf(stderr, "Cannot open file %s\n", s_path);
+      exit(0);
+    }
+
+  for(INT i = 0; i < i_vertices; i++) 
+    {
+      read_line(in, buffer, BUFSIZE);
+      partition[i] = atoi(buffer);
+    }
 
   free(buffer);
-  return (0);
   fclose(in);
+  
+  return (0);
 }
 
-int writePartition(
-  INT i_vertices, 
-  INT * partition, 
-  const char * file_path){
-    
+/**
+ * @brief Write a file describing a partition.
+ *
+ * @param i_vertices       Number of vertices.
+ * @param partition        Partition assignation.
+ * @param s_path           Path of file. 
+ *
+ * @return Integer.
+ */
+int 
+write_partition(INT          i_vertices, 
+                INT        * partition, 
+                const char * file_path)
+{
   char * buffer = malloc(BUFSIZE);
   MEM_ERROR(buffer);
 
-  char file_name[strlen(file_path)+5];
+  char file_name[strlen(file_path) + 5];
   strcpy(file_name, file_path);
   strcat(file_name,".sol");
 
-
   FILE * out = NULL;
   out = fopen(file_name, "w+");
-  if (out == NULL) {
-    fprintf(stderr, "Cannot open file %s\n", file_name);
-    exit(0);
-  }
+  
+  if (out == NULL) 
+    {
+      fprintf(stderr, "Cannot open file %s\n", file_name);
+      exit(0);
+    }
 
-  for(INT i=0; i < i_vertices; i++) {
-    fprintf(out,"%d\n",partition[i]);
-  }
+  for(INT i = 0; i < i_vertices; i++) 
+    {
+      fprintf(out,"%d\n",partition[i]);
+    }
 
   fclose(out);
   free(buffer);
+  
   return (0);
 }
