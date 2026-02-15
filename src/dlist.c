f1cbeaaa7221bcb86a25e87167ae5c2f178d8810
Julien Rodriguez <julien.rodriguez@lirmm.fr>
2026-02-15 11:02:48 +0100

    Refactor header documentation in dlist.c for clarity and consistency

--- src/dlist.c
+++ src/dlist.c
@@ -22,26 +22,27 @@
 */
 
 
-/************************************************************/
-/**                                                        **/
-/**   NAME       : list.c                                  **/
-/**                                                        **/
-/**   AUTHOR     : Julien RODRIGUEZ                        **/
-/**                                                        **/
-/**   FUNCTION   : These lines are double linked list      **/
-/**                functions definitions.                  **/
-/**                                                        **/
-/**   DATES      : # Version 0.0  : from : 10 jun 2022     **/
-/**                                 to   : xx xxx xxxx     **/
-/**                                                        **/
-/**                                                        **/
-/************************************************************/
-
-
+/** 
+ * @file dlist.c
+ * @author Julien Rodriguez
+ * @date 10 jun 2022 – 13 oct 2023
+ * @brief  Double linked list functions declarations for red-black hypergraph
+ *         partitioning algorithms.    
+ */
 #include "dlist.h"
 
-void new_dlist(Dlist * l) {
-
+/**
+ * @brief Function initializing a new double linked list. 
+ * The default value of the root is $-1$ with a size 0 (because 
+ * l was not already assigned).  
+ *
+ * @param l         Double linked list. 
+ *
+ * @return void.
+ */
+void 
+new_dlist(Dlist * l) 
+{
     l->i    = -1;
     l->size = 0;
     l->next = NULL;
@@ -49,47 +50,92 @@ void new_dlist(Dlist * l) {
     return;
 }
 
-void add_dlist(Dlist * la, Dlist * lb){
-    if (la != NULL && lb != NULL) {
+/**
+ * @brief Function doing the concatenation between two double
+ * linked lists ($l_a$ and $l_b$). The result is $l_a + l_b$. The head of lb
+ * following the last element of $l_a$.  
+ *
+ * @param la       Double linked list. 
+ * @param lb       Double linked list.
+ *
+ * @return void.
+ */
+void 
+add_dlist(Dlist * la, 
+          Dlist * lb)
+{
+    if (la != NULL && lb != NULL) 
+      {
         la->next = lb;
         la->size += lb->size;
         lb->pred = la;
-    }
+      }
+    
     return;
 }
 
-void dlist_add_element(Dlist * la, INT i){
-    if (la != NULL) {
-
-        if (la->size>0) {
+/**
+ * @brief Function adding an element (i) to a double linked list
+ * of integers (la). 
+ *
+ * @param la      Double linked list. 
+ * @param i       Integer.
+ *
+ * @return void.
+ */
+void 
+dlist_add_element(Dlist * la, 
+                  INT     i)
+{
+    if (la != NULL) 
+      {
+        if (la->size > 0) 
+          {
             Dlist * lb = (Dlist*)malloc(sizeof(Dlist));
             MEM_ERROR(lb);
             new_dlist(lb);
-            lb->i = i;
-            lb->size=1;
+            lb->i    = i;
+            lb->size = 1;
             Dlist * cell = la;
-            while(cell->next != NULL) {
+            
+            while(cell->next != NULL) 
+              {
                 cell = cell->next;
-            }
+              }
+            
             cell->next = lb;
-            la->size +=1;
-            lb->pred = cell;
-        } else {
-            la->i = i;
-            la->size+=1;
-        }
-    }
+            la->size  += 1;
+            lb->pred   = cell;
+          } 
+        else 
+          {
+            la->i     = i;
+            la->size += 1;
+          }
+      }
+    
     return;
 }
 
-void delete_dlist(Dlist * l) {
-
+/**
+ * @brief Function deleting the list l. 
+ *
+ * @param l      Double linked list. 
+ *
+ * @return void.
+ */
+void 
+delete_dlist(Dlist * l) 
+{
     Dlist * tmp;
-    while (l != NULL) {
+    
+    while (l != NULL) 
+      {
 
         tmp = l->next;
         free(l);
         l   = tmp;
-    }
+      }
+    
     return;
 }
