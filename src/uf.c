fad1bb71fa5a225f7718d6815f4bfd59a39201ef
Julien Rodriguez <julien.rodriguez@lirmm.fr>
2026-02-15 11:08:19 +0100

    Refactor uf.c for improved documentation and consistency in function descriptions

--- src/uf.c
+++ src/uf.c
@@ -22,61 +22,106 @@
 */
 
 
-/************************************************************/
-/**                                                        **/
-/**   NAME       : uf.c                                    **/
-/**                                                        **/
-/**   AUTHOR     : Julien RODRIGUEZ                        **/
-/**                                                        **/
-/**   FUNCTION   : These lines are union-find              **/
-/**                functions definitions.                  **/
-/**                                                        **/
-/**   DATES      : # Version 0.0  : from : 10 jun 2022     **/
-/**                                                        **/
-/**                                                        **/
-/**                                                        **/
-/************************************************************/
-
+/** 
+ * @file uf.c
+ * @author Julien Rodriguez
+ * @date 10 jun 2022 – 13 oct 2023
+ * @brief  These lines are union-find functions definitions    
+ *        for red-black hypergraph. 
+ *      
+ */
 #include "uf.h"
 
-Union_find * new_set(INT element) {
-    Union_find * set = (Union_find*)malloc(sizeof(Union_find));
-    MEM_ERROR(set);
-    set->value      = element;
-    set->parent     = NULL;
-    set->depth      = 1;
-    return set;
+/**
+ * @brief Function creating a new union find structure (set),
+ * containing the integer "element".
+ *
+ * @param element          Integer. 
+ * 
+ * @return Integer.
+ */
+Union_find * 
+new_set(INT element) 
+{
+  Union_find * set = (Union_find*)malloc(sizeof(Union_find));
+    
+  MEM_ERROR(set);
+    
+  set->value      = element;
+    
+  set->parent     = NULL;
+    
+  set->depth      = 1;
+    
+  return set;
 }
 
-Union_find * find    (Union_find * element) {
-    if(!element) {
-        return NULL;
+/**
+ * @brief Function find an element inside the 
+ * union-find structure (set).
+ *
+ * @param element          Union find. 
+ * 
+ * @return Union find.
+ */
+Union_find * 
+find(Union_find * element) 
+{
+  if(!element) 
+    {
+      return NULL;
     }
-    Union_find * base_element = element;
-    while(element->parent != NULL) {
-        element = element->parent;
+    
+  Union_find * base_element = element;
+    
+  while(element->parent != NULL) 
+    {
+      element = element->parent;
     }
-    if (element != base_element->parent && element != base_element) {
-        base_element->parent = element;
+  
+  if (element != base_element->parent && element != base_element) 
+    {
+      base_element->parent = element;
     }
-    return element;
+  
+  return element;
 }
 
-Union_find * merge   (Union_find * a, Union_find * b) {
+/**
+ * @brief Function merging two union find (sets).
+ *
+ * @param a          Union find. 
+ * @param b          Union find. 
+ * 
+ * @return Union find (the new root).
+ */
+Union_find * 
+merge(Union_find * a,
+      Union_find * b) 
+{
+  Union_find * root_a   = find(a);
 
-    Union_find * root_a   = find(a);
-    Union_find * root_b   = find(b);
-    Union_find * new_root = root_a;
+  Union_find * root_b   = find(b);
+  
+  Union_find * new_root = root_a;
 
-    if(root_a != root_b){
-        if(root_a->depth > root_b->depth){
-            root_b->parent = root_a;
-            root_a->depth++;
-        }else{
+  if(root_a != root_b)
+    {
+      if(root_a->depth > root_b->depth)
+        {
+          root_b->parent = root_a;
+            
+          root_a->depth++;
+        }
+      else
+        {
             root_a->parent = root_b;
+            
             root_b->depth++;
+            
             new_root = root_b;
         }
     }
-    return new_root;
+    
+  return new_root;
 }
