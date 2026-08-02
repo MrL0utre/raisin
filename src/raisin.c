73c84a6c3ea2098374ef20cf94c708d840669386
Julien Rodriguez <julien.rodriguez@lirmm.fr>
2026-08-02 21:24:35 +0200

    fix(memory): release contracted CCP partition

--- src/raisin.c
+++ src/raisin.c
@@ -844,6 +844,7 @@ int main(int argv, char ** argc){
            
            rbh_free(h2);
            free(h2);
+           free(partitionp);
            free(map);
            free(is_in);
          }
