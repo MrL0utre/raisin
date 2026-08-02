9561d194749abd5ae3be53d837b70a5849261f6f
Julien Rodriguez <julien.rodriguez@lirmm.fr>
2026-08-02 21:12:55 +0200

    fix(clang): disambiguate negative assignments

--- src/ipart.c
+++ src/ipart.c
@@ -111,8 +111,8 @@ derived_breadth_first_search(Hypergraph * h,
         indeg[i]       = in_neighbors[i]->size;
         flag[i]        = false;
         is_red[i]      = false;
-        pqueue[i]      =- 1;
-        main_pqueue[i] =- 1;
+        pqueue[i]      = -1;
+        main_pqueue[i] = -1;
       }
     
     for(i = 0; i < nr; i++) 
@@ -342,9 +342,9 @@ derived_breadth_first_search_multilevel(Hypergraph * h,
         indeg[i]       = in_neighbors[i]->size;
         flag[i]        = false;
         is_red[i]      = false;
-        pqueue[i]      =- 1;
-        main_pqueue[i] =- 1;
-        partition[i]   =- 1;
+        pqueue[i]      = -1;
+        main_pqueue[i] = -1;
+        partition[i]   = -1;
       }
 
     for(i = 0; i < nr; i++) 
@@ -868,7 +868,7 @@ derived_depth_first_search(Hypergraph * h,
         indeg[i]      = in_neighbors[i]->size;
         flag[i]       = false;
         is_red[i]     = false;
-        pqueue[i]     =- 1;
+        pqueue[i]     = -1;
       }
 
     for(i = 0; i < nr; i++)
@@ -1399,7 +1399,7 @@ critical_connected_component_partitioning(Hypergraph * h,
         outdeg[i]     = out_neighbors[i]->size;
         flag[i]       = false;
         is_red[i]     = false;
-        pqueue[i]     =-1;
+        pqueue[i]     = -1;
       }
     
     for(i = 0; i < nr; i++) 
@@ -1855,7 +1855,7 @@ critical_connected_component_partitioning_multilevel(Hypergraph * h,
         outdeg[i]     = out_neighbors[i]->size;
         flag[i]       = false;
         is_red[i]     = false;
-        pqueue[i]     =-1;
+        pqueue[i]     = -1;
       }
     
     for(i = 0; i < nr; i++)
